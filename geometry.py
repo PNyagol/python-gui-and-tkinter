e5ea63f831448fd176a462e7ddfbe15977a4b27b
PNyagol <nyagolpeter@gmail.com>
2025-03-29 08:38:03 +0300

    latest

---
+++ geometry.py
@@ -0,0 +1,42 @@
+#Geometry managers controling the application layout in Tkinter are:
+# .pack()
+# .place()
+# .grid()
+
+
+
+# Start with .pack()
+import tkinter as tk
+
+window = tk.Tk()
+
+frame1 = tk.Frame(master=window, width=200, height=100, bg="red")
+frame1.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
+
+frame2 = tk.Frame(master=window, width=100, bg="yellow")
+frame2.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
+
+frame3 = tk.Frame(master=window, width=50, bg="blue")
+frame3.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
+
+
+#next is .place() || spacifies the exact position a widget will occupy
+
+frame = tk.Frame(master=window, width=150, height=150)
+frame.pack()
+
+label1 = tk.Label(master=frame, text="I'm at (0, 0)", bg="red")
+label1.place(x=0, y=0)
+
+label2 = tk.Label(master=frame, text="I'm at (75, 75)", bg="yellow")
+label2.place(x=75, y=75)
+
+
+#next is . grid() || the best of the two
+
+# on its own script
+
+
+window.mainloop()
+
+
